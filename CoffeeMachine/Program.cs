7edbc158abd7d4280cdafed534be50f54f30fac1
Chris Joyce <56563947+tigreau@users.noreply.github.com>
2026-02-27 21:20:45 +0200

    Initial commit

---
+++ CoffeeMachine/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeMachine;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ SieMarket/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SieMarket;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
